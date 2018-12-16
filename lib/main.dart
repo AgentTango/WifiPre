545ce6fda03a31b554123bf9ec2b2f250ffb28af
Aditya Thakur <aditya1999thakur@gmail.com>
2018-12-16 19:01:34 +0530

    Redesign w/ DropDowns

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,95 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(new MaterialApp(
-    title: "Wifi Connector",
-    home: new HomePage(),
+void main () {
+  runApp(MaterialApp(
+    title: "Wifi Selector",
+    home: new Scaffold(
+      appBar: AppBar(title: Text("Wifi Selector"),leading: IconButton(icon: Icon(Icons.menu), onPressed: null),),
+      body: new Container(
+        child: SettingsWidget(),
+      ),
+    ),
   ));
 }
 
-class HomePage extends StatelessWidget {
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//        body: new Column(
-//      children: <Widget>[
-//        new Expanded(
-//            child: new Container(
-//          child: new CustomScrollView(
-//            scrollDirection: Axis.vertical,
-//            slivers: <Widget>[
-//              new SliverAppBar(
-//
-//                expandedHeight: 150.0,
-//                backgroundColor: Colors.transparent,
-//              )
-//            ],
-//          ),
-//        ))
-//      ],
-//    ));
+class SettingsWidget extends StatefulWidget {
+  SettingsWidget({Key key}) : super(key: key);
+
+  @override
+  _SettingsWidgetState createState() => new _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+
+  List _cities =
+  ["Cluj-Napoca", "Bucuresti", "Timisoara", "Brasov", "Constanta"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+  String _currentCity;
+
+  @override
+  void initState() {
+    _dropDownMenuItems = getDropDownMenuItems();
+    _currentCity = _dropDownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>> items = new List();
+    for (String city in _cities) {
+      items.add(new DropdownMenuItem(
+          value: city,
+          child: new Text(city)
+      ));
+    }
+    return items;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              //TODO : Have it like the History of Everything app's Title.
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                collapseMode: CollapseMode.parallax,
-                title: Text("Setting Up Wifi",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    )),
-                background: Image.network(
-                  "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
-                  fit: BoxFit.cover,
-                ),
+    return new Container(
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        color: Colors.white,
+        elevation: 4.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Text("Select the wifi :"),
+                  new DropdownButton(
+                    value: _currentCity,
+                    items: _dropDownMenuItems,
+                    onChanged: changedDropDownItem,
+                  ),
+                ],
               ),
-            ),
-          ];
-        },
-        body: Builder(
-          builder: (BuildContext context) {
-            return ListView(
-              children: <Widget>[
-                new Card(
-                  child: new ListTile(
-                    title: Text("Uno", overflow: TextOverflow.fade,),
-                    subtitle: null, //Say Connected if Connected to this bitch try ? : code block
-                    leading: Icon(Icons.wifi_lock),
-                    trailing: Icon(Icons.lock_outline),
-                    onTap: (){
-                      //TODO : check if any password is required, if it is the following has to be called
-                      //TODO : convert this into a function, which takes name and the requiredPassword fields/strings ?
-                      showDialog(context: context,
-                          builder: (_) => new AlertDialog(
-                            content: new Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                new Text("Type the password :"),
-                                new TextField(obscureText: true,),
-                              ],
-                            ),
-                            actions: <Widget>[
-                              new RaisedButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text("Cancel"),
-                                color: Colors.redAccent,
-                                textColor: Colors.white,),
-                              new RaisedButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: Text("Submit"),
-                                color: Colors.blueAccent,
-                                textColor: Colors.white,)
-                            ],
-                          )
-                      );
-                    },
+              new ListTile(
+                  title: new TextField(
+                    obscureText: true,
+                    decoration: InputDecoration.collapsed(hintText: "Enter Password",),
                   ),
-                ),
-                new Card(
-                  child: new ListTile(
-                    title: Text("Uno", overflow: TextOverflow.fade,),
-                    subtitle: null, //Say Connected if Connected to this bitch try ? : code block
-                    leading: Icon(Icons.wifi_lock),
-                    trailing: Icon(Icons.lock_outline),
-                    onTap: (){
-                      //TODO : check if any password is required, if it is the following has to be called
-                      //TODO : convert this into a function, which takes name and the requiredPassword fields/strings ?
-                      showDialog(context: context,
-                        builder: (_) => new AlertDialog(
-                          content: new Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              new Text("Type the password :"),
-                              new TextField(obscureText: true,),
-                            ],
-                          ),
-                          actions: <Widget>[
-                            new RaisedButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: Text("Cancel"),
-                              color: Colors.redAccent,
-                              textColor: Colors.white,),
-                            new RaisedButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: Text("Submit"),
-                              color: Colors.blueAccent,
-                              textColor: Colors.white,)
-                          ],
-                        )
-                      );
-                    },
-                  ),
-                ),
-              ],
-            );
-          }
-        )
+                  trailing: new RaisedButton(
+                      onPressed: () {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text("Button Pressed")));
+                      },
+                      child: new Text("Submit"))
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  void changedDropDownItem(String selectedCity) {
+    setState(() {
+      _currentCity = selectedCity;
+    });
+  }
+
 }
